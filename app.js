853b9cb34cdb8578b1f1a2f756961641a8ab16db
h0996g <h0996g@gmail.com>
2024-01-11 22:44:05 +0100

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+require('dotenv').config();
+
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');//installer le module mongoose (avant vous devez installer et configurer Mongodb)
 const apiRouter = require('./routes/api');
@@ -6,22 +8,44 @@ const apiRouter = require('./routes/api');
 const app = express();
 
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000
+
 
 // Connexion à la base de données MongoDB
 mongoose.set('strictQuery', true);
 
-mongoose.connect('mongodb://127.0.0.1:27017/java', { useNewUrlParser: true })
-  .then(() => {
-    console.log('Connexion réussie à la base de données');
+// mongoose.connect('mongodb://127.0.0.1:27017/java', { useNewUrlParser: true })
+//   .then(() => {
+//     console.log('Connexion réussie à la base de données');
 
 
 
-    app.use('/api', apiRouter);
+//     app.use('/api', apiRouter);
 
-    app.listen(3000, () => {
-      console.log('Server started on port 3000');
-    });
+//     app.listen(3000, () => {
+//       console.log('Server started on port 3000');
+//     });
+//   })
+//   .catch((error) => {
+//     console.log(`Erreur de connexion à la base de données : ${error}`);
+//   });
+
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(PORT);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+app.use('/', apiRouter);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("listening for requests");
   })
-  .catch((error) => {
-    console.log(`Erreur de connexion à la base de données : ${error}`);
-  });
+})
